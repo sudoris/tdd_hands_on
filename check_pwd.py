cee8bfde0f69dc501bef286947e3cc6ab23c41d3
sudoris <zdent09@gmail.com>
2020-11-10 14:31:09 +0800

    modify check_pwd to reject pwd if pwd only contains digits

--- check_pwd.py
+++ check_pwd.py
@@ -1,7 +1,9 @@
-def check_pwd(pwd):        
+def check_pwd(pwd):         
     if len(pwd) < 8:
         return False
     if len(pwd) > 20:
+        return False
+    if pwd.isdigit():
         return False
 
     return True
