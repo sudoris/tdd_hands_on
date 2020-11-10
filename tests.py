3319f7ca372872f5c8894262723efb658d3f582b
sudoris <zdent09@gmail.com>
2020-11-10 13:48:43 +0800

    start from scratch by removing boilerplate test cases and importing the check_pwd function from the check_pwd file

--- tests.py
+++ tests.py
@@ -1,33 +1,10 @@
 import unittest
-from task import fizz_buzz
+from check_pwd import check_pwd
 
 
 class TestCase(unittest.TestCase):
   
-    def test1(self):
-        input = 1
-        expected = '1'
-        self.assertEqual(fizz_buzz(input), expected)
-
-    def test2(self):
-        input = 2
-        expected = '2'
-        self.assertEqual(fizz_buzz(input), expected)
-
-    def test3(self):
-        input = 3
-        expected = 'fizz'
-        self.assertEqual(fizz_buzz(input), expected)
-
-    def test5(self):
-        input = 5
-        expected = 'buzz'
-        self.assertEqual(fizz_buzz(input), expected)
-
-    def test6(self):
-        input = 6
-        expected = 'fizz'
-        self.assertEqual(fizz_buzz(input), expected)
+    
 
 if __name__ == '__main__':
     unittest.main()
